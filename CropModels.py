dc7aac5ce447165b0fb7a5e293c0c10d595300dc
JiNx <249589575@qq.com>
2022-02-11 17:20:44 +0800

    Add files via upload
    add googlenet moblienet shufflenet

--- CropModels.py
+++ CropModels.py
@@ -16,6 +16,7 @@ densenet161 = M.densenet161
 densenet201 = M.densenet201
 
 
+
 class ResNetFinetune(nn.Module):
     finetune = True
 
@@ -89,6 +90,59 @@ class FinetunePretrainedmodels(nn.Module):
         return self.net(x)
 
 
+class ShuffleNetV2Finetune(nn.Module):
+    def __init__(self, num_classes, net_cls=M.shufflenet_v2_x0_5):
+        super().__init__()
+        self.net = net_cls(pretrained=True)
+        self.net.fc = nn.Linear(self.net.fc.in_features, num_classes)
+
+    def fresh_params(self):
+        return self.net.fc.parameters()
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class GoogleNetv2Finetune(nn.Module):
+    def __init__(self, num_classes, net_cls=M.googlenet):
+        super().__init__()
+        self.net = net_cls(pretrained=True)
+        self.net.fc = nn.Linear(self.net.fc.in_features, num_classes)
+
+    def fresh_params(self):
+        return self.net.fc.parameters()
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class MobileNetv3Fintune(nn.Module):
+    def __init__(self, num_classes, model_type='small'):
+        super().__init__()
+        if model_type == "small":
+            self.net = M.mobilenet_v3_small(pretrained=True)
+        elif model_type == "large":
+            self.net = M.mobilenet_v3_large(pretrained=True)
+
+        removed_sequential = list(self.net.classifier.children())
+        fc = nn.Linear(removed_sequential[-1].in_features, num_classes)
+        removed_sequential[-1] = fc
+        self.net.classifier = nn.Sequential(*removed_sequential)
+        self.fresh_param = fc.parameters()
+
+    def fresh_params(self):
+        return self.fresh_param
+
+    def forward(self, x):
+        return self.net(x)
+
+
+
+
+
+
+
+
 resnet18_finetune = partial(ResNetFinetune, net_cls=M.resnet18)
 resnet34_finetune = partial(ResNetFinetune, net_cls=M.resnet34)
 resnet50_finetune = partial(ResNetFinetune, net_cls=M.resnet50)
@@ -99,6 +153,9 @@ densenet121_finetune = partial(DenseNetFinetune, net_cls=M.densenet121)
 
 densenet161_finetune = partial(DenseNetFinetune, net_cls=M.densenet161)
 densenet201_finetune = partial(DenseNetFinetune, net_cls=M.densenet201)
+shufflenetv2_finetune = partial(ShuffleNetV2Finetune, net_cls=M.shufflenet_v2_x0_5)
+googlenetv2_finetune = partial(GoogleNetv2Finetune, net_cls=M.googlenet)
+mobilenetv3_finetune = partial(MobileNetv3Fintune, model_type='small')
 
 xception_finetune = partial(FinetunePretrainedmodels,
                             net_cls=pretrainedmodels.xception,
@@ -119,3 +176,4 @@ nasnet_finetune = partial(FinetunePretrainedmodels,
 nasnetmobile = partial(FinetunePretrainedmodels,
                        net_cls=pretrainedmodels.nasnetamobile,
                        net_kwards={'pretrained': 'imagenet', 'num_classes': 1000})
+
--- train.py
+++ train.py
@@ -31,18 +31,19 @@ model_dict = {
     'resnet152': resnet152_finetune,
     'densenet121': densenet121_finetune,
     'densenet161': densenet161_finetune,
-    'ddensenet201': densenet201_finetune,
+    'densenet201': densenet201_finetune,
     'xception': xception_finetune,
     'inceotionv4': inceptionv4_finetune,
     'inceptionv2': inceptionresnetv2_finetune,
     'nasnet': nasnet_finetune,
-    'nasnetmobile': nasnetmobile
+    'nasnetmobile': nasnetmobile,
+    'mobilenet': mobilenetv3_finetune,
+    'googlenet': googlenetv2_finetune,
+    'shufflenet': shufflenetv2_finetune
 }
 
 
 def train(epochNum, model_name='resnet18'):
-    assert model_name in ['resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152',
-                          'vgg16']
     # 初始化tensorboad
     date = str(datetime.date.today())
     # 创建 /log/日期/InceptionResnet的组织形式  不同模型需要修改不同名称
@@ -59,7 +60,7 @@ def train(epochNum, model_name='resnet18'):
 
     train_dataloader, val_dataloader = split_dataset_pytorch(dataset, batch_size=BATCH_SIZE, validation_split=validation_split, random_seed=random_seed)
 
-    model = model_dict['resnet18'](num_classes=NB_CLASS).cuda()
+    model = model_dict[model_name](num_classes=NB_CLASS).cuda()
 
     criterion = nn.CrossEntropyLoss().cuda()
     lx, px = utils.predict(model, val_dataloader)
@@ -69,28 +70,28 @@ def train(epochNum, model_name='resnet18'):
     patience = 0
     # momentum = 0.0
     lr = 1e-4
-    optimizer = torch.optim.Adam(model.fresh_params(), lr=lr, amsgrad=True, weight_decay=1e-4)
-    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizer, milestones=[20, 30, 40], gamma=0.1)
+    optimizer_warming = torch.optim.Adam(model.fresh_params(), lr=lr, amsgrad=True, weight_decay=1e-4)
+    optimizer_training = torch.optim.Adam(model.parameters(), lr=lr)
+    scheduler_warming = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizer_training, milestones=[20, 30, 40], gamma=0.1)
     for epoch in range(epochNum):
         print('Epoch {}/{}'.format(epoch, epochNum - 1))
         print('-' * 10)
-        # if epoch == 3:
-        #     lr = 1e-4
-        #     momentum = 0.9
-        #     print('set lr=:%f,momentum=%f' % (lr, momentum))
+
         if patience == 2:
             patience = 0
             model.load_state_dict(torch.load('save_model/' + model_name + "/loss_best/" + date + '_loss_best.pth')['state_dict'])
             lr = lr / 10
             print('loss has increased lr divide 10 lr now is :%f' % (lr))
-        # if epoch == 0 or epoch == 1 or epoch == 2:  # 第一轮首先训练全连接层
-        #     optimizer = torch.optim.Adam(model.fresh_params(), lr=1e-3, amsgrad=True, weight_decay=1e-4)
-        #     scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizer, milestones=[20, 30, 40], gamma=0.1)
-        # else:
-        #     optimizer = torch.optim.Adam(model.parameters(), lr=lr, amsgrad=True, weight_decay=1e-4)
-        #     scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizer, milestones=[17, 27, 27], gamma=0.1)
+        if epoch < 10:  # 第一轮首先训练全连接层
+            optimizer = optimizer_warming
+            scheduler = None
+        else:
+            optimizer = optimizer_training
+            scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizer, milestones=[17, 27, 27], gamma=0.1)
+
         running_loss = RunningMean()
         running_corrects = RunningMean()
+
         train_bar = tqdm(train_dataloader)
         train_bar.desc = "train_step"
         for batch_idx, (inputs, labels) in enumerate(train_bar):
@@ -112,7 +113,8 @@ def train(epochNum, model_name='resnet18'):
             scaler.scale(loss).backward()
             scaler.step(optimizer)
             scaler.update()
-            scheduler.step()
+            if scheduler is not None:
+                scheduler.step()
 
         #
         # val
@@ -150,10 +152,15 @@ def train(epochNum, model_name='resnet18'):
                         'state_dict': model.state_dict(),
                         'optimizer': optimizer.state_dict(),
                         'val_loss': log_loss,
-                        'val_correct': accuracy}, 'save_model/' + model_name + '/acc_beat/' + date + '_acc_best.pth')
+                        'val_correct': accuracy}, 'save_model/' + model_name + '/acc_best/' + date + '_acc_best.pth')
             min_acc = accuracy
             print('save new model acc,now acc is ', min_acc)
 
 
 if __name__ == '__main__':
-    train(100)
+    # model_list = ['resnet34', 'resnet50', 'resnet101', 'resnet152', 'densenet121', 'densenet161', 'densenet201',
+    #               'xception', 'inceotionv4', 'inceptionv2', 'nasnet', 'nasnetmobile', 'mobilenet', 'googlenet',
+    #               'shufflenet']
+    # for model_name in model_list:
+    #     train(100, model_name)
+    train(100, "googlenet")
